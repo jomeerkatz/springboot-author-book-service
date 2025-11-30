e551e7592c9fd75162ba2100e8d1b6724db0ed1a
jomeerkatz <jokirillmeerkatz@outlook.de>
2025-11-30 07:58:12 +0100

    create readAll endpoints for books and authors + integration tests for status and body

--- src/main/java/com/jomeerkatz/springboot_backend_cc/controllers/AuthorController.java
+++ src/main/java/com/jomeerkatz/springboot_backend_cc/controllers/AuthorController.java
@@ -6,9 +6,13 @@ import com.jomeerkatz.springboot_backend_cc.mappers.Mapper;
 import com.jomeerkatz.springboot_backend_cc.service.AuthorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AuthorController {
@@ -28,5 +32,14 @@ public class AuthorController {
         AuthorEntity createResult = authorService.createAuthor(authorEntity);
         return new ResponseEntity<>(authorMapper.mapTo(createResult), HttpStatus.CREATED
         );
+    }
+
+    @GetMapping(path = "/authors")
+    public List<AuthorDto> getAllAuthors() {
+        List<AuthorEntity> authorsList = authorService.getAllAuthors();
+        return authorsList
+                .stream()
+                .map(authorMapper::mapTo)
+                .toList();
     }
 }
